e95c9b85d1f551cb70c1fbbb32d3481bbfc17808
wujianfeirun <275940822@qq.com>
2016-09-18 21:57:15 +0800

    muti-modules under parent module; rpm  included

---
+++ ParentModule/rpmModule/target/classes/service/scripts/ableinstall/postinstall.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo *******************************
+echo ****      postinstall      ****
+echo *******************************
+
+echo install_home=${RPM_INSTALL_PREFIX0}
+echo env=${RPM_INSTALL_PREFIX1}
+echo java_home=${RPM_INSTALL_PREFIX2}
---
+++ ParentModule/springModule/src/main/java/com/main/beanRef/Son.java
@@ -0,0 +1,13 @@
+package com.main.beanRef;
+
+public class Son {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
