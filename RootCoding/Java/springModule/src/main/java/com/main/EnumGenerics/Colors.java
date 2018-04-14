9c4bd1cf2f2ffe67463c5fc9d4b25f2fb5cdfdd6
wu275 <275940822>
2018-04-14 11:38:41 +0800

    clean the repository

---
+++ RootCoding/Java/springModule/src/main/java/com/main/EnumGenerics/Colors.java
@@ -0,0 +1,15 @@
+package com.main.EnumGenerics;
+
+public enum Colors {
+	RED,WHITE,BLACK,BLUE;
+	private int colorNum;
+
+	public int getColorNum() {
+		return colorNum;
+	}
+
+	public void setColorNum(int colorNum) {
+		this.colorNum = colorNum;
+	}
+	
+}
