3c7d82dee728289b7ed85789dbec412c1bac7645
wu275 <275940822>
2018-04-24 22:34:08 +0800

    rebuild the repository

---
+++ RootCoding/Java/MultiThread/src/main/java/com/main/_04Lambda/LambLearn.java
@@ -0,0 +1,23 @@
+package com.main._04Lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * parallelStream 是并行处理的； 底层使用的是fork/join
+ * stream是串行执行的
+ */
+
+public class LambLearn {
+    //rebase branch
+    public static void main(String[] args) {
+        List<Integer> values = Arrays.asList(10,20,30,40);
+        int res = new LambLearn().add(values);
+        System.out.println(res);
+    }
+
+    public int add(List<Integer> values){
+        return values.parallelStream().mapToInt(a -> a).sum();
+    }
+
+}
